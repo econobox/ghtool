7fa8a5ad3b03845ff565dd4db2af24cd53a6d0a7
Søren Mortensen <soren@sorenmortensen.com>
2018-02-28 20:16:16 +0000

    Initial commit (migrating ghlabelcpy, starting with args)

---
+++ README.md
@@ -0,0 +1,14 @@
+# ghtool
+
+`ghtool` is a command-line tool for interacting with all the extra
+data associated with a GitHub repository that doesn't come from `git`
+itself.
+
+## Note
+
+This is a side project. I'm mainly driven to implement features in
+`ghtool` by my own need for them in my workflow. That said, though, if
+there's a feature that you'd like to see implemented, feel free to
+file an issue and I'm likely to work with you to get it working (or at
+least consider it seriously). I'll certainly accept pull requests if
+you develop a feature yourself.
---
+++ ghtool-label/src/copy/mod.rs
@@ -0,0 +1,96 @@
+//
+//  copy/mod.rs
+//  ghtool-label
+//
+//  Created by Søren Mortensen on 28/02/2018.
+//  Copyright © 2018 Søren Mortensen.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+/// Details about this command.
+pub mod details {
+    use clap::{App, Arg};
+
+    /// This command's app definition.
+    pub fn app() -> App<'static, 'static> {
+        App::new(name())
+            .version(version())
+            .author(author())
+            .about(description())
+            .args(&args()[..])
+    }
+
+    /// This command's name.
+    fn name() -> &'static str { "copy" }
+
+    /// This command's version.
+    fn version() -> &'static str { env!("CARGO_PKG_VERSION") }
+
+    /// This command's author(s).
+    fn author() -> &'static str { env!("CARGO_PKG_AUTHORS") }
+
+    /// This command's description.
+    fn description() -> &'static str { "Copy labels from one repository to another" }
+
+    /// This command's arguments.
+    fn args() -> Vec<Arg<'static, 'static>> {
+        vec![
+            // --from
+            Arg::with_name("from")
+                .short("f")
+                .long("from")
+                .value_name("FROM")
+                .help("The repository to copy from, in the format \"user/repository\"")
+                .takes_value(true)
+                .required(true),
+            // --to
+            Arg::with_name("to")
+                .short("t")
+                .long("to")
+                .value_name("TO")
+                .help("The repository to copy to, in the format \"user/repository\"")
+                .takes_value(true)
+                .required(true),
+            // --token
+            Arg::with_name("token")
+                .long("token")
+                .value_name("TOKEN")
+                .help(
+                    "GitHub personal access token that provides access to the repositories specified by <FROM> and \
+                    <TO>. Overrides any existing value from ~/.config/ghtool/config.toml"
+                )
+                .takes_value(true)
+                .required(true), //(!Config::file_exists()),
+            // --clear
+            Arg::with_name("clear")
+                .short("c")
+                .long("clear")
+                .help(
+                    "Clear the existing labels from the repository specified by <TO> before copying the new ones"
+                ),
+            // --yes
+            Arg::with_name("yes")
+                .short("y")
+                .long("yes")
+                .help(
+                    "Automatic yes to prompts; assume \"yes\" as an answer to all prompts and run non-interactively.",
+                ),
+            // -v, -vv, -vvv
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity (up to -vvv)"),
+        ]
+    }
+}
